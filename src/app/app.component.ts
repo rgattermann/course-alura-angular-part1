bb30b7f7e6a156da292afad3dbb4037995722808
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2019-04-10 11:41:04 -0300

    files from lessions 1 and 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Course Alura Angular';
+  photos = [
+    {
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      imageDescription: 'Leão'
+    },
+    {
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      imageDescription: 'Leoa'
+    }
+  ];
 }
