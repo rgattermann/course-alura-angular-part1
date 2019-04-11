6704db1968a53946e62c966c429b95e8bea9bf99
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2019-04-11 10:50:17 -0300

    files from lession 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Course Alura Angular';
-  photos: Object[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos)
-  }
 }
